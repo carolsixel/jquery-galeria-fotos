4319b9af01c118399bda4dd60d875893883c5a12
Caroline Sixel <caroline.sixels@gmail.com>
2024-11-01 08:59:22 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#cancel-button').click(function () {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const addressNewImage = $('#address-new-image').val();
+        const newItem = $('<li style="display: none"></li>');
+        $(`<img src="${addressNewImage}" />`).appendTo(newItem);
+        $(`
+            <div class="overlay-image-link">
+                <a href="${addressNewImage}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+            `).appendTo(newItem);
+        $(newItem).appendTo('ul');
+        $(newItem).fadeIn(1000);
+        $('#address-new-image').val('');
+    })
+})
